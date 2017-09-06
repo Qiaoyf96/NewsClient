91a83173d3bec857cd04c230daab61fe9811a5ff
PWrau <vinwrau@163.com>
2017-09-07 02:35:10 +0800

    add recyclerView VRecents

--- app/src/main/java/com/newsclient/data/DSingleNews.java
+++ app/src/main/java/com/newsclient/data/DSingleNews.java
@@ -5,7 +5,7 @@ public class DSingleNews {
             news_id, news_pictures, news_source, news_time,
             news_title, news_url, news_video, news_intro, content;
     public boolean readed;
-    DSingleNews(){
+    public DSingleNews(){
         content = "";
         readed = false;
     }
---
+++ app/src/main/java/com/newsclient/view/VRecyclerView.java
@@ -0,0 +1,105 @@
+package com.newsclient.view;
+
+/**
+ * show news items on recent list
+ *
+ *
+ */
+import com.newsclient.R;
+import com.newsclient.data.DSingleNews;
+
+import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VRecyclerView {
+
+    private DSingleNews[] news;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private Activity activity;
+    private int targetLayout;
+    private int sourceLayout;
+    private int[] itemsId;
+
+    VRecyclerView(DSingleNews[] news, Activity activity, int sourceLayout, int targetLayout, int[] itemsId){
+        this.news = news;
+        this.activity = activity;
+        this.sourceLayout = sourceLayout;
+        this.targetLayout = targetLayout;
+        this.itemsId = itemsId;
+    }
+
+    void generate(){
+        mRecyclerView = (RecyclerView) activity.findViewById(targetLayout);
+        mLayoutManager = new LinearLayoutManager(activity);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter = new RecyclerAdapter());
+    }
+
+
+    class RecyclerShowItemGroup{
+        TextView title;
+        TextView source;
+
+        RecyclerShowItemGroup(View[] args){
+            title = (TextView) args[0];
+            source = (TextView) args[1];
+        }
+
+        void bindValue(int index){
+            title.setText(news[index].news_title);
+            source.setText(news[index].news_source);
+        }
+    }
+
+    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.InnerViewHolder>{
+        @Override
+        public InnerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            InnerViewHolder holder = new InnerViewHolder(LayoutInflater.from(
+                    activity).inflate(sourceLayout, parent,
+                    false),
+                    itemsId);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(InnerViewHolder holder, int position) {
+
+            holder.item.bindValue(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return news.length;
+        }
+
+        class InnerViewHolder extends RecyclerView.ViewHolder
+        {
+            RecyclerShowItemGroup item;
+
+            public InnerViewHolder(View view, int[] args)
+            {
+                super(view);
+                this.initContentList(view, args);
+            }
+
+            void initContentList(View parentView, int[] args){
+                View[] views = new View[args.length];
+                for (int i = 0; i < args.length; i++){
+                    views[i] = parentView.findViewById(args[i]);
+                }
+                item = new RecyclerShowItemGroup(views);
+            }
+        }
+    }
+}
