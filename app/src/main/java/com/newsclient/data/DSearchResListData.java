8d9023c949cf93d1f11a5806514167741752a944
Xembo <809642754@qq.com>
2017-09-08 15:32:57 +0800

    Search Feature
    Search Tab, search feature, and search view

---
+++ app/src/main/java/com/newsclient/data/DSearchResListData.java
@@ -0,0 +1,44 @@
+package com.newsclient.data;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Comparator;
+
+import static com.newsclient.tools.Http.sendGet;
+
+/**
+ * Created by xemboliu on 8/9/2017.
+ */
+
+public class DSearchResListData {
+    public ArrayList<String> _news_title_list = new ArrayList<String>();
+    public ArrayList<String> _news_id_set = new ArrayList<String>();
+    public int _size;
+    public int _total;
+
+    public void load(String query, int pagecnt) {
+        int pageSize = 5 << pagecnt;
+        String str;
+        if (pagecnt == 1)
+            str = sendGet("http://166.111.68.66:2042/news/action/query/search?keyword=" + query + "&pageSize=20&pageNo=1");
+        else
+            str = sendGet("http://166.111.68.66:2042/news/action/query/search?keyword=" + query + "&pageSize=" + pageSize + "&pageNo=2");
+        try {
+            JSONArray artList = new JSONArray(new JSONObject(str).getString("list"));
+            _size = artList.length();
+            for (int i = 0; i < _size; i++) {
+                JSONObject art = artList.getJSONObject(i);
+                DSingleNews news = new DSingleNews(art);
+                _news_id_set.add(art.getString("news_ID"));
+                _news_title_list.add(art.getString("news_Title"));
+            }
+            _total = Integer.parseInt(new JSONObject(str).getString("totalRecords"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
