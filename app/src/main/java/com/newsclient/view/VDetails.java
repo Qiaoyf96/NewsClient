efe0791b597a83a2c3ebb42931ebb8abead89c11
Yifan <LegaDyan@gmail.com>
2017-09-08 12:07:15 +0800

    add news reader(tts)

--- app/src/main/java/com/newsclient/view/VDetails.java
+++ app/src/main/java/com/newsclient/view/VDetails.java
@@ -3,7 +3,10 @@ package com.newsclient.view;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
 import android.widget.TextView;
 
 import com.newsclient.R;
@@ -11,6 +14,10 @@ import com.newsclient.data.DNewsList;
 import com.newsclient.data.DSingleNews;
 
 public class VDetails extends Activity {
+    TextToSpeech tts;
+    FloatingActionButton btn;
+    TextView tv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +29,23 @@ public class VDetails extends Activity {
         DSingleNews news = DNewsList.getById(id);
         news.load();
 
-        TextView tv = (TextView)findViewById(R.id.textView2);
+        tv = (TextView)findViewById(R.id.textView2);
         tv.setText(news.content);
 
+        tts = new TextToSpeech(this, null);
+        btn = findViewById(R.id.floatingActionButton);
+        //实例化
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tts.speak(tv.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+                //语音输出
+            }});
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tts.stop();
     }
 }
