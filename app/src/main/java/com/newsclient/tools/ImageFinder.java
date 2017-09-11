9f123c4f92c889c90ea3154e0014d96f01f460f9
Xembo <809642754@qq.com>
2017-09-11 13:04:36 +0800

    Delete Test output for ImageFinder

--- app/src/main/java/com/newsclient/tools/ImageFinder.java
+++ app/src/main/java/com/newsclient/tools/ImageFinder.java
@@ -1,14 +1,7 @@
 package com.newsclient.tools;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.StrictMode;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
@@ -25,12 +18,10 @@ public class ImageFinder {
             String finalurl;
             String content;
             urlarray = new ArrayList<String>();
-            System.out.println(keyword);
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
             String searchurl = "http://image.baidu.com/search/avatarjson?tn=resultjsonavatarnew&ie=utf-8&word=" + keyword + "&cg=star&pn=0&rn=30&itg=0&z=0&fr=&width=&height=&lm=-1&ic=0&s=0&st=-1&gsm=0";
             content = Http.sendGet(searchurl);
-            System.out.println(content);
             Pattern pt = Pattern.compile("thumbURL\"\\:\"(.+?)\"");
             Matcher match = pt.matcher(content);
             while(match.find()){
@@ -38,7 +29,6 @@ public class ImageFinder {
                 urlarray.add(s);
             }
             int idx = 1;
-            System.out.println(urlarray.size());
             if (urlarray.size() < 2) idx = urlarray.size() - 1;
             finalurl = urlarray.get(idx);
 
