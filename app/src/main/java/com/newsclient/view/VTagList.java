b8796f4f73e7140710a21bc2561412b903e88ae1
PWrau <vinwrau@163.com>
2017-09-11 14:39:48 +0800

    Add card view.

--- app/src/main/java/com/newsclient/view/VTagList.java
+++ app/src/main/java/com/newsclient/view/VTagList.java
@@ -7,13 +7,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.newsclient.R;
+import com.newsclient.data.DSingleNews;
 import com.newsclient.data.DSingleTag;
 import com.newsclient.data.DTagList;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class VTagList extends Activity {
     int tagId;
+    //final DSingleTag dt;
 
     @Override
     protected void onResume() {
@@ -26,7 +34,15 @@ public class VTagList extends Activity {
         }
 
         ListView lv = (ListView) findViewById(R.id.listViewTag);
-        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titleList));
+        //lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titleList));
+
+
+        // simpleAdapter
+        SimpleAdapter adapter = new SimpleAdapter(this, getItemData(dt), R.layout.activity_cardview,
+                new String[] {"card_title", "card_source", "card_intropic", "card_intro"},
+                new int[] {R.id.card_title, R.id.card_source, R.id.card_intropic, R.id.card_intro});
+
+        lv.setAdapter(adapter);
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -43,7 +59,28 @@ public class VTagList extends Activity {
             }
         });
 
+
+
         super.onResume();
+    }
+
+    private List<Map<String, Object>> getItemData(DSingleTag tag){
+        // data need:
+        // String card_title
+        // String card_source
+        // Bitmap card_intropic
+        // String card_intro
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (DSingleNews news : tag.news_list){
+            Map<String, Object> map = new HashMap<>();
+            map.put("card_title", news.displayTitle());
+            map.put("card_source", news.displaySource() + "  " + news.displayTime());
+            map.put("card_intropic", news.news_intropic);
+            map.put("card_intro", news.news_intro);
+            list.add(map);
+        }
+
+        return list;
     }
 
     @Override
